fe5347a7a0828bee190079b983dcd176cab14960
Khairul <khairulukd@gmail.com>
2026-03-07 12:36:29 +0600

    Add implementation of various operators in Python

---
+++ operator.py
@@ -0,0 +1,62 @@
+
+# addition, substraction, multiplication, division, exponentiation -------------------
+
+print(2 + 3)
+print(2 - 3)
+print(2 * 3)
+print(2 / 3) # always return float value
+print(2**2)
+
+
+# Comperison operator ---------------------------
+
+
+num1 = 10
+num2 = 20
+print(num1 < num2)
+print(num1 > num2)
+print(num1 == num2)
+print(num1 != num2)
+print(num1 >= num2)
+print(num1 <= num2)
+
+
+# Membership operator -------------------------------
+
+
+str1 = 'khairul'
+print('kh' in str1)
+number = [10, 20, 30, 40]
+print('50' in number)
+print('50' not in number)
+
+
+# Identity Operator ----------------------------------
+
+
+a = 100
+b = 100
+print(a is b)
+print(a is not b)
+
+
+# Logical Operator -------------------------------
+
+
+print(True and True)
+print(False or True)
+print(not False)
+
+
+# Bitwise Operator --------------------------------
+
+
+a = 5
+b = 3
+print(a & b)
+print(a | b)
+print(a ^ b)
+print(~a)
+print(bin(a))
+print(5 << 2)
+print(3 >> 1)
